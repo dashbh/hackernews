66017779c2975586c275d40175207c61b108b8d2
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2020-04-17 10:07:49 +0530

    Seo Fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import './index.scss';
 
 const Header = () => (
   <header>
-    <img className="logo" width="18" src="/favicon.ico" />
+    <img className="logo" alt="Logo" width="18" src="/favicon.ico" />
     <NavBar />
   </header>
 );
--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -10,35 +10,22 @@ const Story = (props) => {
   return (
     <li className="story">
       <span className="story__comment-count">
-        {' '}
         {story.num_comments ? story.num_comments : ''}
-        {' '}
       </span>
       <span className="story__upvote-count">
-        {' '}
         {story.points ? `${story.points}.` : ''}
-        {' '}
       </span>
       <span className="story__upvote" onClick={() => onUpvote(story)} />
       <span className="story__title">{story.story_title ? story.story_title : story.title}</span>
       {story.hideUserDetails ? '' : (
         <span className="story__meta">
           {story_url ? `(${story_url})` : ''}
-          &nbsp; by
-          {' '}
-          <b>{story.author}</b>
-          &nbsp;
-          {' '}
-          {moment(story.created_at).fromNow()}
+          &nbsp; by&nbsp;<b>{story.author}</b>&nbsp;{moment(story.created_at).fromNow()}
         </span>
       )}
-      <a className="story__toggle-user" onClick={() => onTggleUserDetails(story)}>
-        [
-        {' '}
-        {story.hideUserDetails ? 'show' : 'hide'}
-        {' '}
-        ]
-      </a>
+      <span className="story__toggle-user" onClick={() => onTggleUserDetails(story)}>
+        [{story.hideUserDetails ? 'show' : 'hide'}]
+      </span>
     </li>
   );
 };
